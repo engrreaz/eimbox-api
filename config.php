ad703d326a6dfa553d5848274f8c4fbce6bb6cc1
Reazul Hoque <engrreaz@gmail.com>
2025-12-11 01:49:06 +0600

    console-fix

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'secure_auth');
+define('DB_USER', 'eimbox_eimbox');
+define('DB_PASS', '@Riaz2015');
+define('DB_NAME', 'eimbox_databasesystem');
 define('DB_PORT', 3306);           // যদি আলাদা পোর্ট হয় তবে বদলাবেন
 define('DB_CHARSET', 'utf8mb4');   // utf8mb4 রাখাই ভালো (emoji সহ)
